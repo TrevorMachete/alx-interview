3ebefd0503bbad574860f4849681b3409f4794cc
Macs Machete <trevormachete@gmail.com>
2024-06-13 17:34:19 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""
-This module contains a function to determine if all boxes can be opened.
-"""
+"""Script will unlock list of lists"""
 
-def can_unlock_all(boxes):
-    """Determine if all boxes can be opened."""
-    # Start with the first box unlocked
-    unlocked_boxes = {0}
-    keys = set(boxes[0])
 
-    # While we have keys that could potentially open new boxes
-    while keys:
-        new_keys = set()
-        for key in keys:
-            # If the key corresponds to a box
-            if key < len(boxes) and key not in unlocked_boxes:
-                # Unlock the box
-                unlocked_boxes.add(key)
-                # Add the keys from this box
-                new_keys.update(boxes[key])
-        keys = new_keys
+def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
 
-    # If the number of unlocked boxes is equal to the total number of boxes
-    # then all boxes can be opened
-    return len(unlocked_boxes) == len(boxes)
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
